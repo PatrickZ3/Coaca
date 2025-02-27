6873767a87171ded5ac6a68a9279ebf507b2da28
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-02-27 12:48:57 -0500

    wtf

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Coaca', // Set this to your repo name
+  assetPrefix: '/Coaca/',
+  trailingSlash: true, // Ensures correct routing
 };
 
 export default nextConfig;
